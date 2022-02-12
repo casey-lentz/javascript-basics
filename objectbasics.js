a65216c1bc6507e9da0a2fa400f1bc6679ac3dec
clentz <clentz@newsbank.com>
2022-02-12 10:55:35 -0800

    first object note

---
+++ objectbasics.js
@@ -0,0 +1,17 @@
+let object1 = {}; // create empty object1 and store anything
+object1['name'] = 'John'; // key:value pair
+object1['age'] = 20;
+console.log(object1); // representation of data in the key:value
+console.log(JSON.stringify(object1)); // json stringify put in string
+
+let stringifiedJSON = JSON.stringify(object1);
+
+let javascriptObject = JSON.parse(stringifiedJSON); // string back to object
+console.log(stringifiedJSON); // string
+console.log(javascriptObject); // object
+
+object1['print'] = function() { // values of key:value pairs can be functions
+  console.log('print from function');
+}
+object1.print(); // call the function
+
